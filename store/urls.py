7fc87643d5a0ec65d539518e7d726ecb880fd65a
MariomcgeeArt <mariomcgeeart@gmail.com>
2021-01-12 19:42:35 -0600

    updated cart value

--- store/urls.py
+++ store/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 
 urlpatterns = [
-
-    #Leave as empty string for base url
+	#Leave as empty string for base url
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
 
-]
+	path('update_item/', views.updateItem, name="update_item"),
 
+]
 
